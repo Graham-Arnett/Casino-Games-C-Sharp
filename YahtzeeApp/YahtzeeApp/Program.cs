e890587d84bde72c272c0dc0cb83db254fdf8b57
Graham Arnett <GrahamKentArnett@outlook.com>
2025-03-03 19:41:48 -0500

    init commit yahtzee, not done yet

---
+++ YahtzeeApp/YahtzeeApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace YahtzeeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my Yahtzee App!");
+            Random random = new Random();
+            string choice;
+            do
+            {
+                //funny story, I don't know the rules of yahtzee, so what I'm gonna do is just make the skeleton structure tonight, and do the rest tomorrow
+                for(int i = 0; i < 5; i++)
+                {
+                    //apparently you use 5 dice, so I figure each time this goes you roll one of the dice?
+                    //also I want to try my hand at printing out ascii images of the dice faces when I have everything else working
+                }
+                Console.Write("Would you like to play again? (y/n): ");
+            choice= Console.ReadLine().ToLower();
+            } while (choice == "y");
+        }
+    }
+}
