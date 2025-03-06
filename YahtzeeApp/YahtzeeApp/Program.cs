a2369730622d95c59ffce9c1aebf71962421d64e
Graham Arnett <GrahamKentArnett@outlook.com>
2025-03-06 13:26:46 -0500

    not a lot today, mainly pseudocode planning out how to approach this

--- YahtzeeApp/YahtzeeApp/Program.cs
+++ YahtzeeApp/YahtzeeApp/Program.cs
@@ -7,12 +7,16 @@
             Console.WriteLine("Welcome to my Yahtzee App!");
             Random random = new Random();
             string choice;
+            int rounds = 13;//apparently yahtzee is 13 rounds long
+            int points = 0;//initialize points at zero, different combos are worth different amounts
             do
             {
 
                 //funny story, I don't know the rules of yahtzee, so what I'm gonna do is just make the skeleton structure tonight, and do the rest tomorrow
                 for(int i = 0; i < 5; i++)
                 {
+                    //this forloop should probably be inside another dowhile since it says a player can roll their dice up to 3 times on their turn
+                    //so like itd have the counter for the times in the condition, as well as if they say 'y' to the prompt
                     int roll = random.Next(1,7);//lower bound is inclusive, upper is exclusive so its 1-6 really
                     Console.Write($"\tYour roll: {roll}"); //eventually I want to print out ascii dice here
                     //should the other player have a separate forloop? nah probably not
