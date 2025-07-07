517fe50478be19723744dc8bddc11455b7207496
Graham Arnett <GrahamKentArnett@outlook.com>
2025-07-07 14:11:51 -0400

    Removed bot rolls, this is a no dealer version theres no point in them being there

--- YahtzeeApp/YahtzeeApp/Program.cs
+++ YahtzeeApp/YahtzeeApp/Program.cs
@@ -20,7 +20,7 @@
                                 "|      |\n" +
                                 "|   O  |\n" +
                                 "--------"};
-            Console.WriteLine($"{dice[0]} \t {dice[1]}");
+            //Console.WriteLine($"{dice[0]} \t {dice[1]}");
             do
             {
 
@@ -32,59 +32,58 @@
                     int roll = random.Next(1,7);//lower bound is inclusive, upper is exclusive so its 1-6 really
                     Console.Write($"\tYour roll: {roll}"); //eventually I want to print out ascii dice here
                     //should the other player have a separate forloop? nah probably not
-                    int botRolls = random.Next(1, 7);//this probably violates DRY, but I'm still learning so its ok
-                    Console.WriteLine($"\tThe other player rolled: {botRolls}");//ok this formatting needs work
-                    switch (roll)
-                    {
-                        case 1:
-                            Console.Write("\t" +
-                                "--------\n" +
-                                "|      |\n" +
-                                "|   O  |\n" +
-                                "|      |\n" +
-                                "--------\n" );
-                            break;
-                        case 2:
-                            Console.Write("" +
-                                "--------\n" +
-                                "|   O  |\n" +
-                                "|      |\n" +
-                                "|   O  |\n" +
-                                "--------\n");
-                            break;
-                        case 3:
-                            Console.Write("\t" +
-                                "--------\n" +
-                                "|   O  |\n" +
-                                "|   O  |\n" +
-                                "|   O  |\n" +
-                                "--------\n");
-                            break;
-                        case 4:
-                            Console.Write("\t" +
-                                "--------\n" +
-                                "| O  O |\n" +
-                                "|      |\n" +
-                                "| O  O |\n" +
-                                "--------\n");
-                            break;
-                        case 5:
-                            Console.Write("\t" +
-                                "--------\n" +
-                                "| O  O |\n" +
-                                "|   O  |\n" +
-                                "| O  O |\n" +
-                                "--------\n");
-                            break;
-                        case 6:
-                            Console.Write("\t" +
-                                "--------\n" +
-                                "| O  O |\n" +
-                                "| O  O |\n" +
-                                "| O  O |\n" +
-                                "--------\n");
-                            break;
-                    }
+                   
+                    //switch (roll)
+                    //{
+                    //    case 1:
+                    //        Console.Write("\t" +
+                    //            "--------\n" +
+                    //            "|      |\n" +
+                    //            "|   O  |\n" +
+                    //            "|      |\n" +
+                    //            "--------\n" );
+                    //        break;
+                    //    case 2:
+                    //        Console.Write("" +
+                    //            "--------\n" +
+                    //            "|   O  |\n" +
+                    //            "|      |\n" +
+                    //            "|   O  |\n" +
+                    //            "--------\n");
+                    //        break;
+                    //    case 3:
+                    //        Console.Write("\t" +
+                    //            "--------\n" +
+                    //            "|   O  |\n" +
+                    //            "|   O  |\n" +
+                    //            "|   O  |\n" +
+                    //            "--------\n");
+                    //        break;
+                    //    case 4:
+                    //        Console.Write("\t" +
+                    //            "--------\n" +
+                    //            "| O  O |\n" +
+                    //            "|      |\n" +
+                    //            "| O  O |\n" +
+                    //            "--------\n");
+                    //        break;
+                    //    case 5:
+                    //        Console.Write("\t" +
+                    //            "--------\n" +
+                    //            "| O  O |\n" +
+                    //            "|   O  |\n" +
+                    //            "| O  O |\n" +
+                    //            "--------\n");
+                    //        break;
+                    //    case 6:
+                    //        Console.Write("\t" +
+                    //            "--------\n" +
+                    //            "| O  O |\n" +
+                    //            "| O  O |\n" +
+                    //            "| O  O |\n" +
+                    //            "--------\n");
+                    //        break;
+                    //}
                 }
                 Console.Write("\n\nWould you like to play again? (y/n): ");
             choice= Console.ReadLine().ToLower();
