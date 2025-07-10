639277e84d61dc9f2572aa904f0eae8102201beb
Graham Arnett <GrahamKentArnett@outlook.com>
2025-07-09 22:02:22 -0400

    still working, got rid of the ascii it just wasn't needed

--- YahtzeeApp/YahtzeeApp/Program.cs
+++ YahtzeeApp/YahtzeeApp/Program.cs
@@ -9,81 +9,24 @@
             string choice;
             int rounds = 13;//apparently yahtzee is 13 rounds long
             int points = 0;//initialize points at zero, different combos are worth different amounts
-            int[] roundRolls = new int[5]; //the idea is you can have stuff like straights in yahtzee, so order is important, therefore we can do the checking on index value
-            //string[] dice = {"--------\n" +
-            //                 "|      |\n" +
-            //                 "|   O  |\n" +
-            //                 "|      |\n" +
-            //                 "--------",
-            //                    "--------" +
-            //                    "|   O  |\n" +
-            //                    "|      |\n" +
-            //                    "|   O  |\n" +
-            //                    "--------"};
-            //Console.WriteLine($"{dice[0]} \t {dice[1]}");
+            
             do
             {
-
+                int[] roundRolls = new int[5];
+                Console.WriteLine($"Your rolls: ");
                 //funny story, I don't know the rules of yahtzee, so what I'm gonna do is just make the skeleton structure tonight, and do the rest tomorrow
-                for(int i = 0; i < 5; i++)
+                for (int i = 0; i < 5; i++)
                 {
                     //this forloop should probably be inside another dowhile since it says a player can roll their dice up to 3 times on their turn
                     //so like itd have the counter for the times in the condition, as well as if they say 'y' to the prompt
                     int roll = random.Next(1,7);//lower bound is inclusive, upper is exclusive so its 1-6 really
-                    Console.Write($"\tYour roll: {roll}"); //eventually I want to print out ascii dice here
-                    //should the other player have a separate forloop? nah probably not
-                   
-                    //switch (roll)
-                    //{
-                    //    case 1:
-                    //        Console.Write("\t" +
-                    //            "--------\n" +
-                    //            "|      |\n" +
-                    //            "|   O  |\n" +
-                    //            "|      |\n" +
-                    //            "--------\n" );
-                    //        break;
-                    //    case 2:
-                    //        Console.Write("" +
-                    //            "--------\n" +
-                    //            "|   O  |\n" +
-                    //            "|      |\n" +
-                    //            "|   O  |\n" +
-                    //            "--------\n");
-                    //        break;
-                    //    case 3:
-                    //        Console.Write("\t" +
-                    //            "--------\n" +
-                    //            "|   O  |\n" +
-                    //            "|   O  |\n" +
-                    //            "|   O  |\n" +
-                    //            "--------\n");
-                    //        break;
-                    //    case 4:
-                    //        Console.Write("\t" +
-                    //            "--------\n" +
-                    //            "| O  O |\n" +
-                    //            "|      |\n" +
-                    //            "| O  O |\n" +
-                    //            "--------\n");
-                    //        break;
-                    //    case 5:
-                    //        Console.Write("\t" +
-                    //            "--------\n" +
-                    //            "| O  O |\n" +
-                    //            "|   O  |\n" +
-                    //            "| O  O |\n" +
-                    //            "--------\n");
-                    //        break;
-                    //    case 6:
-                    //        Console.Write("\t" +
-                    //            "--------\n" +
-                    //            "| O  O |\n" +
-                    //            "| O  O |\n" +
-                    //            "| O  O |\n" +
-                    //            "--------\n");
-                    //        break;
-                    //}
+                    roundRolls[i] = roll;
+                    Console.Write($"{roundRolls[i]}\t");
+                    
+                }
+                if (roundRolls[1] == roundRolls[0] + 1 && roundRolls[2] == roundRolls[1] + 1 && roundRolls[3] == roundRolls[2] + 1 && roundRolls[4] == roundRolls[3] + 1)
+                {
+                    Console.WriteLine("You got a STRAIGHT!");
                 }
                 Console.Write("\n\nWould you like to play again? (y/n): ");
             choice= Console.ReadLine().ToLower();
